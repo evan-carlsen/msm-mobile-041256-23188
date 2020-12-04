495461424ab0e694583b3a2c917633429cb0746a
manan-crowdbotics <61427302+manan-crowdbotics@users.noreply.github.com>
2020-12-04 15:42:21 +0530

    Update mainNavigator.js

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -6,6 +6,7 @@ import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import CbSplashScreen1182121Navigator from '../features/CbSplashScreen1182121/navigator';
+import HomeScreen3182119Navigator from '../features/HomeScreen3182119/navigator';
 /**
  * new navigators can be imported here
  */
@@ -16,7 +17,8 @@ const AppNavigator = {
 
     /** new navigators can be added here */
     SplashScreen: { screen: SplashScreen},
-    CbSplashScreen1182121: { screen: CbSplashScreen1182121Navigator }
+    CbSplashScreen1182121: { screen: CbSplashScreen1182121Navigator },
+    HomeScreen3182119: { screen: HomeScreen3182119Navigator }
 };
 
 const DrawerAppNavigator = createDrawerNavigator(
