305159052fdf2228712c0984bad000fae3690ec8
evan-carlsen <60202545+evan-carlsen@users.noreply.github.com>
2020-12-04 16:33:41 -0800

    Update mainNavigator.js

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -14,11 +14,12 @@ import HomeScreen3182119Navigator from '../features/HomeScreen3182119/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
-
-    /** new navigators can be added here */
-    SplashScreen: { screen: SplashScreen},
     CbSplashScreen1182121: { screen: CbSplashScreen1182121Navigator },
     HomeScreen3182119: { screen: HomeScreen3182119Navigator }
+    
+    /** new navigators can be added here */
+    SplashScreen: { screen: SplashScreen},
+
 };
 
 const DrawerAppNavigator = createDrawerNavigator(
